337fa337a08cd79e21f25f9749de4f6e2d6b6868
Kent Knox (xN/A) TX <kent.knox@amd.com>
2012-06-22 12:27:04 -0600

    ECR #333753 - Removing refernence to sequentialTrait.h, as it is not used yet Modified cmake files to download a local copy of boost, with much faster downloads (hosted on see-srv) Benchmark program now queries AMP for all devices, and the user can select the device from command line parameter
    [git-p4: depot-paths = "//depot/stg/hsa/drivers/hsa/boltProject/": change = 785010]

--- bench/AMP/Scan/scan.cpp
+++ bench/AMP/Scan/scan.cpp
@@ -5,27 +5,52 @@
 //#include <bolt/AMP/systemQuery.h>
 #include <bolt/unicode.h>
 
-void printAccelerator( const concurrency::accelerator& dev )
+void printAccelerator( unsigned int num, const concurrency::accelerator& dev )
 {
-	const std::streamsize width = 25;
+	const std::streamsize width = 26;
 
-	bolt::tout << std::setw( width ) << _T( "Description: " ) << dev.get_description( ) << std::endl;
-	bolt::tout << std::setw( width ) << _T( "Device Path: " ) << dev.get_device_path( ) << std::endl;
-	bolt::tout << std::setw( width ) << _T( "Version: " ) << dev.get_version( ) << std::endl;
-	bolt::tout << std::setw( width ) << _T( "Dedicated Memory: " ) << dev.get_dedicated_memory( ) << std::endl;
-	bolt::tout << std::setw( width ) << _T( "Double support: " ) << dev.get_supports_double_precision( ) << std::endl;
-	bolt::tout << std::setw( width ) << _T( "Limited Double Support: " ) << dev.get_supports_limited_double_precision( ) << std::endl;
-	bolt::tout << std::setw( width ) << _T( "Emulated: " ) << dev.get_is_emulated( ) << std::endl;
-	bolt::tout << std::setw( width ) << _T( "Debug: " ) << dev.get_is_debug( ) << std::endl;
-	bolt::tout << std::setw( width ) << _T( "Display: " ) << dev.get_has_display( ) << std::endl;
+	std::wcout << std::left << std::boolalpha;
+	std::wcout << std::setw( width ) << _T( "Device: " ) << num << std::endl;
+	std::wcout << std::setw( width ) << _T( "Description: " ) << dev.get_description( ) << std::endl;
+	std::wcout << std::setw( width ) << _T( "Device Path: " ) << dev.get_device_path( ) << std::endl;
+	std::wcout << std::setw( width ) << _T( "Version: " ) << dev.get_version( ) << std::endl;
+	std::wcout << std::setw( width ) << _T( "Dedicated Memory: " ) << std::showpoint << dev.get_dedicated_memory( ) / 1024 << _T( " MB" ) << std::endl;
+	std::wcout << std::setw( width ) << _T( "Double support: " ) << dev.get_supports_double_precision( ) << std::endl;
+	std::wcout << std::setw( width ) << _T( "Limited Double Support: " ) << dev.get_supports_limited_double_precision( ) << std::endl;
+	std::wcout << std::setw( width ) << _T( "Emulated: " ) << dev.get_is_emulated( ) << std::endl;
+	std::wcout << std::setw( width ) << _T( "Debug: " ) << dev.get_is_debug( ) << std::endl;
+	std::wcout << std::setw( width ) << _T( "Display: " ) << dev.get_has_display( ) << std::endl;
 
-	bolt::tout << std::endl;
+	concurrency::accelerator_view av = dev.get_default_view( );
+	std::wcout << std::setw( width ) << _T( "Default accelerator view: " ) << std::endl;
+	std::wcout << std::setw( width ) << _T( "    Version: " ) << av.get_version( ) << std::endl;
+	std::wcout << std::setw( width ) << _T( "    Debug: " ) << av.get_is_debug( ) << std::endl;
+
+	concurrency::queuing_mode qm = av.get_queuing_mode( );
+	bolt::tout << std::setw( width ) << _T( "    Queueing mode: " );
+	switch( qm )
+	{
+		case concurrency::queuing_mode_immediate:
+			std::wcout << _T( "immediate" );
+			break;
+		case concurrency::queuing_mode_automatic:
+			std::wcout << _T( "automatic" );
+			break;
+		default:
+			std::wcout << _T( "unknown" );
+			break;
+	}
+	std::wcout << std::endl;
+
+
+	std::wcout << std::internal << std::noboolalpha << std::endl;
 }
 
 int _tmain( int argc, _TCHAR* argv[] )
 {
 	size_t length = 0;
 	size_t iDevice = 0;
+	bool choseDevice = false;
 
 	try
 	{
@@ -36,7 +61,7 @@ int _tmain( int argc, _TCHAR* argv[] )
 			( "version,v",		"Print queryable version information from the Bolt AMP library" )
 			( "ampInfo,i",		"Print queryable information of the AMP runtime" )
 			( "length,l",		po::value< size_t >( &length )->default_value( 4096 ), "Specify the length of scan array" )
-			( "gpu,g",			po::value< size_t >( &iDevice )->default_value( 0 ), "Choose specific AMP device, otherwise system default (AMP choose)" )
+			( "device,d",		po::value< size_t >( &iDevice ), "Choose specific AMP device, otherwise system default (AMP choose)" )
 			;
 
 		po::variables_map vm;
@@ -45,7 +70,7 @@ int _tmain( int argc, _TCHAR* argv[] )
 
 		if( vm.count( "version" ) )
 		{
-			//	Todo:  Query Bolt for its version information
+			//	TODO:  Query Bolt for its version information
 			size_t libMajor, libMinor, libPatch;
 			libMajor = 0;
 			libMinor = 0;
@@ -65,12 +90,22 @@ int _tmain( int argc, _TCHAR* argv[] )
 			return 0;
 		}
 
-		std::vector< concurrency::accelerator > allDevices = concurrency::accelerator::get_all( );
-
 		if( vm.count( "ampInfo" ) )
 		{
-			std::for_each( allDevices.begin( ), allDevices.end( ), printAccelerator );
+			std::vector< concurrency::accelerator > allDevices = concurrency::accelerator::get_all( );
+
+			//std::for_each( allDevices.begin( ), allDevices.end( ), printAccelerator );
+			for( unsigned int i = 0; i < allDevices.size( ); ++i )
+				printAccelerator( i, allDevices.at( i ) );
+
+			return 0;
 		}
+
+		if( vm.count( "device" ) )
+		{
+			choseDevice = true;
+		}
+
 	}
 	catch( std::exception& e )
 	{
@@ -79,12 +114,20 @@ int _tmain( int argc, _TCHAR* argv[] )
 	}
 
 //	bolt::control::getDefault( );
-
-
 	std::vector< int > input( length, 1 );
 	std::vector< int > output( length );
 
-	bolt::inclusive_scan( input.begin( ), input.end( ), output.begin( ) );
+	if( choseDevice )
+	{
+		std::vector< concurrency::accelerator > allDevices = concurrency::accelerator::get_all( );
+
+		bolt::inclusive_scan( allDevices.at( iDevice ).get_default_view( ), input.begin( ), input.end( ), 
+					output.begin( ), bolt::plus< int >( ) );
+	}
+	else
+	{
+		bolt::inclusive_scan( input.begin( ), input.end( ), output.begin( ) );
+	}
 
 	return 0;
 }
