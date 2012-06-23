da237030780ee771851deb6cc058db7f161e0de9
Kent Knox (xN/A) TX <kent.knox@amd.com>
2012-06-22 21:51:39 -0600

    ECR #333753 - Functioning bench program for scan, now prints out GB/s to stdout.  Contains command line parameters to set the vector length and loopcount. Checking in a CPU based timer class that calculates the stddev of the samples it takes, and eliminates outlying samples to reduce timing variability
    [git-p4: depot-paths = "//depot/stg/hsa/drivers/hsa/boltProject/": change = 785154]

--- include/bolt/unicode.h
+++ include/bolt/unicode.h
@@ -6,6 +6,23 @@
 #include <iostream>
 #include <sstream>
 
+//	These macros help linux cope with the conventions of windows tchar.h file
+#if defined( _WIN32 )
+	#include <tchar.h>
+#else
+	#if defined( __GNUC__ )
+		typedef char TCHAR;
+		typedef char _TCHAR;
+		#define _tmain main
+
+		#if defined( UNICODE )
+			#define _T(x)	L ## x
+		#else
+			#define _T(x)	x
+		#endif 
+	#endif
+#endif
+
 namespace bolt
 {
 #if defined( _UNICODE )
@@ -14,16 +31,20 @@ namespace bolt
 	typedef std::wifstream		tifstream;
 	typedef std::wofstream		tofstream;
 	typedef std::wfstream		tfstream;
+	typedef std::wostream		tstream;
 	static std::wostream&	tout	= std::wcout;
 	static std::wostream&	terr	= std::wcerr;
+	static std::wostream&	tlog	= std::wclog;
 #else
 	typedef std::string tstring;
 	typedef std::stringstream tstringstream;
 	typedef std::ifstream		tifstream;
 	typedef std::ofstream		tofstream;
 	typedef std::fstream		tfstream;
+	typedef std::ostream		tstream;
 	static std::ostream&	tout	= std::cout;
 	static std::ostream&	terr	= std::cerr;
+	static std::ostream&	tlog	= std::clog;
 #endif 
 }
 
