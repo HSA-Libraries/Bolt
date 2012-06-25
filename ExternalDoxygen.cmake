9a5130dfd1ff8f513c5b210c35b95403e1f7c965
Kent Knox (xN/A) TX <kent.knox@amd.com>
2012-06-25 10:56:09 -0600

    ECR #333753 - Checking in automation for generating documentation; a new project called Bolt.Documentation is defined, which must be explicitely built to invoke doxygen.  It is an independant project, and NOT tied to the all_build project. Since Bolt.Documentation has an external dependency on Doxygen, the cmake project defaults to OFF for this project.  That way, you don't have to download doxygen unless you want it.  In order to see the Bolt.Documentation project in the IDE, you first must configure the CMake project and turn BUILD_documentation on.
    [git-p4: depot-paths = "//depot/stg/hsa/drivers/hsa/boltProject/": change = 785464]

---
+++ ExternalDoxygen.cmake
@@ -0,0 +1,36 @@
+message( STATUS "Setting up Doxygen SuperBuild... Shiny!" )
+include( ExternalProject )
+
+set( Doxygen_Version 1.8.1.1 )
+
+message( STATUS "Doxygen_Version: " ${Doxygen_Version} )
+
+# Purely for debugging the file downloading URLs
+# file( DOWNLOAD "http://ftp.stack.nl/pub/users/dimitri/doxygen-${Doxygen_Version}.windows.bin.zip" 
+		# "${CMAKE_CURRENT_BINARY_DIR}/download/Doxygen-${Doxygen_Version}/Doxygen_1_49_0.7z" SHOW_PROGRESS STATUS fileStatus LOG fileLog )
+# message( STATUS "status: " ${fileStatus} )
+# message( STATUS "log: " ${fileLog} )
+
+# Below is a fancy CMake command to download, build and install Doxygen on the users computer
+ExternalProject_Add(
+    Doxygen
+	PREFIX ${CMAKE_CURRENT_BINARY_DIR}/external/Doxygen
+#    URL http://ftp.stack.nl/pub/users/dimitri/doxygen-${Doxygen_Version}.windows.bin.zip
+    URL http://see-srv/share/code/externals/doxygen/doxygen-${Doxygen_Version}.windows.bin.zip
+	URL_MD5 20c22209c85d3e8cc1d7d59f7c0bf351
+    UPDATE_COMMAND ""
+#    PATCH_COMMAND ""
+	CONFIGURE_COMMAND ""
+	BUILD_COMMAND ""
+	BUILD_IN_SOURCE 1
+    INSTALL_COMMAND ""
+)
+
+set_property( TARGET Doxygen PROPERTY FOLDER "Externals")
+
+ExternalProject_Get_Property( Doxygen source_dir )
+ExternalProject_Get_Property( Doxygen binary_dir )
+
+set( DOXYGEN_EXECUTABLE ${binary_dir}/doxygen.exe )
+# set( Doxygen_INCLUDE_DIRS ${source_dir} )
+# set( Doxygen_LIBRARIES debug;${binary_dir}/stage/lib/libDoxygen_program_options-vc110-mt-gd-1_49.lib;optimized;${binary_dir}/stage/lib/libDoxygen_program_options-vc110-mt-1_49.lib )
