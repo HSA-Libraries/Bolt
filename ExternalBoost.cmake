fb43b7b2abb348934c746fd79c0c0fe378cd86ce
Kent Knox (xN/A) TX <kent.knox@amd.com>
2012-07-06 13:49:46 -0600

    ECR #333753 - Creating cmake 'superbuild' project with Boost & Doxygen.  This seperates building our externals and building Bolt into seperate projects.  First, you open and built the project that downloads and builds boost/doxygen, then you open the project that builds bolt.  Bolt project can now assume that boost and doxygen are preexisting packages, and do not have to rebuild them when doing 'project clean'.  Still need to add googletest and tbb dependencies.  This should now be very simple to do.  Need to update build documentation.
    [git-p4: depot-paths = "//depot/stg/hsa/drivers/hsa/boltProject/": change = 790144]

--- ExternalBoost.cmake
+++ ExternalBoost.cmake
@@ -1,28 +1,50 @@
 message( STATUS "Setting up Boost SuperBuild... Shiny!" )
 include( ExternalProject )
 
-set( Boost_Version 1.49.0 )
-string( REPLACE "." "_" Boost_Version_Underscore ${Boost_Version} )
+set( ext.Boost_VERSION "1.49.0" CACHE STRING "Boost version to download/use" )
+mark_as_advanced( ext.Boost_VERSION )
+string( REPLACE "." "_" ext.Boost_Version_Underscore ${ext.Boost_VERSION} )
 
-message( STATUS "Boost_Version: " ${Boost_Version} )
+message( STATUS "ext.Boost_VERSION: " ${ext.Boost_VERSION} )
 
 # Purely for debugging the file downloading URLs
 # file( DOWNLOAD "http://downloads.sourceforge.net/project/boost/boost/1.49.0/boost_1_49_0.7z" 
-		# "${CMAKE_CURRENT_BINARY_DIR}/download/boost-${Boost_Version}/boost_1_49_0.7z" SHOW_PROGRESS STATUS fileStatus LOG fileLog )
+		# "${CMAKE_CURRENT_BINARY_DIR}/download/boost-${ext.Boost_VERSION}/boost_1_49_0.7z" SHOW_PROGRESS STATUS fileStatus LOG fileLog )
 # message( STATUS "status: " ${fileStatus} )
 # message( STATUS "log: " ${fileLog} )
+
+set( Boost.Command b2 --with-program_options )
+
+if( Bolt_BUILD64 )
+	list( APPEND Boost.Command address-model=64 )
+else( )
+	list( APPEND Boost.Command address-model=32 )
+endif( )
+
+if( MSVC )
+	if( MSVC_VERSION VERSION_LESS 1700 )
+		list( APPEND Boost.Command toolset=msvc-10.0 )
+	else( )
+		list( APPEND Boost.Command toolset=msvc-11.0 )
+	endif( )
+endif( )
+
+list( APPEND Boost.Command link=static stage )
+
+#set( ext.Boost_URL "http://downloads.sourceforge.net/project/boost/boost/${ext.Boost_VERSION}/boost_${ext.Boost_Version_Underscore}.zip" CACHE STRING "URL to download Boost from" )
+set( ext.Boost_URL "http://see-srv/share/code/externals/boost/boost_${ext.Boost_Version_Underscore}.zip" CACHE STRING "URL to download Boost from" )
+mark_as_advanced( ext.Boost_URL )
 
 # Below is a fancy CMake command to download, build and install Boost on the users computer
 ExternalProject_Add(
     Boost
 	PREFIX ${CMAKE_CURRENT_BINARY_DIR}/external/boost
-#    URL http://downloads.sourceforge.net/project/boost/boost/${Boost_Version}/boost_${Boost_Version_Underscore}.zip
-    URL http://see-srv/share/code/externals/boost/boost_${Boost_Version_Underscore}.zip
+    URL ${ext.Boost_URL}
 	URL_MD5 854dcbbff31b896c85c38247060b7713
     UPDATE_COMMAND "bootstrap.bat"
 #    PATCH_COMMAND ""
 	CONFIGURE_COMMAND ""
-	BUILD_COMMAND b2 --with-program_options address-model=64 toolset=msvc-11.0 link=static stage
+	BUILD_COMMAND ${Boost.Command}
 	BUILD_IN_SOURCE 1
     INSTALL_COMMAND ""
 )
@@ -34,13 +56,15 @@ ExternalProject_Get_Property( Boost binary_dir )
 set( Boost_INCLUDE_DIRS ${source_dir} )
 set( Boost_LIBRARIES debug;${binary_dir}/stage/lib/libboost_program_options-vc110-mt-gd-1_49.lib;optimized;${binary_dir}/stage/lib/libboost_program_options-vc110-mt-1_49.lib )
 
+set( Boost_FOUND TRUE )
+
 # Can't get packages to download from github because the cmake file( download ... ) does not understand https protocal
 # Gitorious is problematic because it apparently only offers .tar.gz files to download, which windows doesn't support by default
 # Also, both repo's do not like to append a filetype to the URL, instead they use some forwarding script.  ExternalProject_Add wants a filetype.
 # set( BOOST_BUILD_PROJECTS program_options CACHE STRING "* seperated Boost modules to be built")
 # ExternalProject_Add(
    # Boost
-   # URL http://gitorious.org/boost/cmake/archive-tarball/cmake-${Boost_Version}.tar.gz
+   # URL http://gitorious.org/boost/cmake/archive-tarball/cmake-${ext.Boost_VERSION}.tar.gz
    # LIST_SEPARATOR *
    # CMAKE_ARGS 	-DENABLE_STATIC=ON 
 				# -DENABLE_SHARED=OFF 
