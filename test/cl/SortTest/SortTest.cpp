83bab3710e1098e9a861919a2ff0ebdf35e8c5f3
Ravishekhar Banger (xN/A) BG <ravishekhar.banger@amd.com>
2012-08-10 05:13:30 -0600

    ECR #010005 - fixed a minor ussie inth etest code
    [git-p4: depot-paths = "//depot/stg/hsa/drivers/hsa/boltProject/": change = 803565]

--- test/cl/SortTest/SortTest.cpp
+++ test/cl/SortTest/SortTest.cpp
@@ -36,10 +36,11 @@ int main(int argc, char* argv[])
 
     // using default comparison (operator <):
     std::sort (stdvector.begin(), stdvector.begin()+4);           //(12 32 45 71)26 80 53 33
-    bolt::cl::sort(boltvector.begin(), boltvector.end()+4);           //(12 32 45 71)26 80 53 33
+    bolt::cl::sort(boltvector.begin(), boltvector.begin()+4);           //(12 32 45 71)26 80 53 33
     // using function as comp
     std::sort (stdvector.begin()+4, stdvector.end(), myfunction); // 12 32 45 71(26 33 53 80)
-    bolt::cl::sort (boltvector.begin()+4, boltvector.end(), bolt::cl::maximum<int>()); // 12 32 45 71(26 33 53 80)
+    //bolt::cl::sort (boltvector.begin()+4, boltvector.end(), bolt::cl::maximum<int>()); // 12 32 45 71(26 33 53 80)
+    bolt::cl::sort (boltvector.begin()+4, boltvector.end(), myfunction); // 12 32 45 71(26 33 53 80)
     // using object as comp
     std::sort (stdvector.begin(), stdvector.end(), myobject);     //(12 26 32 33 45 53 71 80)
     //bolt::cl::sort (c, boltvector.begin(), boltvector.end(), myobject);     //(12 26 32 33 45 53 71 80)
