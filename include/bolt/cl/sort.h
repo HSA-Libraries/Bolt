c31597adc04649ddb873644ef601c6a265d5ca2b
Ravishekhar Banger (xN/A) BG <ravishekhar.banger@amd.com>
2012-08-13 08:39:08 -0600

    ECR #010005 - added doxygen comments to the sort interfaces
    [git-p4: depot-paths = "//depot/stg/hsa/drivers/hsa/boltProject/": change = 804233]

--- include/bolt/cl/sort.h
+++ include/bolt/cl/sort.h
@@ -26,36 +26,33 @@ namespace bolt {
 
 
 		/*! \p sort returns the sorted result of all the elements in the inputIterator between the the first and last elements using the specified binary_op.  
-		* You can do an ascending order sorting, where the binary_op is the minimum operator.  By default, 
+		* You can arrange the elements in an ascending order, where the binary_op is the minimum operator.  By default, 
 		* the binary operator is "minimum<>()".  The version takes a bolt::cl::control structure as a first argument.
+
 		*
-		* \p reduce requires that the binary reduction op ("binary_op") is cummutative.  The order in which \p reduce applies the binary_op
-		* is not deterministic.
-		*
-		* The \p reduce operation is similar the std::accumulate function.  See http://www.sgi.com/tech/stl/accumulate.html.
+		* The \p sort operation is similar the std::sort function.  See http://www.sgi.com/tech/stl/sort.html
 		*
 		* \param ctl Control structure to control command-queue, debug, tuning, etc.  See control.
-		* \param first The first position in the sequence to be reduced.
-		* \param last  The last position in the sequence to be reduced.
-		* \param init  The initial value for the accumulator.
-		* \param binary_op  The binary operation used to combine two values.   By default, the binary operation is plus<>().
+		* \param first The first position in the sequence to be sorted.
+		* \param last  The last position in the sequence to be sorted.
+		* \param comp  The comparison operation used to compare two values.   By default, the compare operation is minimum<>(). i.e the elements are stored in ascending order. 
 		* \param cl_code Optional OpenCL(TM) code to be passed to the OpenCL compiler. The cl_code is inserted first in the generated code, before the cl_code trait.
-		* \return The result of the reduction.
+		* \return The sorted data which is available in place.
 		*
-		* The following code example shows the use of \p reduce to find the max of 10 numbers, 
+		* The following code example shows the use of \p sort to sort the elements in the descending order, 
 		* specifying a specific command-queue and enabling debug messages.
 		* \code
-		* #include <bolt/cl/reduce.h>
+		* #include <bolt/cl/sort.h>
 		*
 		* int a[10] = {2, 9, 3, 7, 5, 6, 3, 8, 3, 4};
 		*
 		* cl::CommandQueue myCommandQueue = ...
 		*
-		* bolt::cl::control ctl(myCommandQueue); // specify an OpenCL(TM) command queue to use for executing the reduce.
+		* bolt::cl::control ctl(myCommandQueue); // specify an OpenCL(TM) command queue to use for executing the sort routine.
 		* ctl.debug(bolt::cl::control::debug::SaveCompilerTemps); // save IL and ISA files for generated kernel
-		*
-		* int max = bolt::cl::reduce(ctl, a, a+10, -1, bolt::cl:maximum<int>());
-		* // max = 9
+		* // for arranging th eelements in the descending order, you can use bolt::cl::maximum<int>()
+		* bolt::cl::sort(ctl, a, a+10, bolt::cl::maximum<int>());
+		* // a => {9, 8, 7, 6, 5, 4, 3, 3, 3, 2}
 		*  \endcode
 		*/
 
@@ -68,34 +65,34 @@ namespace bolt {
 
 
 
-		/*! \p reduce returns the result of combining all the elements in the specified range using the specified binary_op.  
-		* The classic example is a summation, where the binary_op is the plus operator.  By default, the initial value is "0" 
-		* and the binary operator is "plus<>()".
+		/*! \p sort returns the sorted result of all the elements in the inputIterator between the the first and last elements using the specified binary_op.  
+		* You can arrange the elements in an ascending order, where the binary_op is the minimum operator.  By default, 
+		* the binary operator is "minimum<>()".  The version takes a bolt::cl::control structure as a first argument.
+
 		*
-		* \p reduce requires that the binary reduction op ("binary_op") is cummutative.  The order in which \p reduce applies the binary_op
-		* is not deterministic.
+		* The \p sort operation is similar the std::sort function.  See http://www.sgi.com/tech/stl/sort.html
 		*
-		* The \p reduce operation is similar the std::accumulate function.  See http://www.sgi.com/tech/stl/accumulate.html.
-		*
-		* \param ctl Control structure to control command-queue, debug, tuning. See FIXME.
-		* \param first The first position in the sequence to be reduced.
-		* \param last  The last position in the sequence to be reduced.
+		* \param ctl Control structure to control command-queue, debug, tuning, etc.  See control.
+		* \param first The first position in the sequence to be sorted.
+		* \param last  The last position in the sequence to be sorted.
 		* \param cl_code Optional OpenCL(TM) code to be passed to the OpenCL compiler. The cl_code is inserted first in the generated code, before the cl_code trait.
-		* \return The result of the reduction.
+		* \return The sorted data which is available in place and is in ascending order.
 		*
-		* The following code example shows the use of \p reduce to sum 10 numbers, using the default plus operator.
+		* Here by default the function does an ascending order sort. 
+        * The following code example shows the use of \p sort to sort the elements in the ascending order, 
+		* specifying a specific command-queue and enabling debug messages.
 		* \code
-		* #include <bolt/cl/reduce.h>
+		* #include <bolt/cl/sort.h>
 		*
-		* int a[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		* int a[10] = {2, 9, 3, 7, 5, 6, 3, 8, 3, 4};
 		*
 		* cl::CommandQueue myCommandQueue = ...
 		*
-		* bolt::cl::control ctl(myCommandQueue); // specify an OpenCL(TM) command queue to use for executing the reduce.
+		* bolt::cl::control ctl(myCommandQueue); // specify an OpenCL(TM) command queue to use for executing the sort routine.
 		* ctl.debug(bolt::cl::control::debug::SaveCompilerTemps); // save IL and ISA files for generated kernel
-		*
-		* int sum = bolt::cl::reduce(ctl, a, a+10);
-		* // sum = 55
+		* // for arranging th eelements in the descending order, you can use bolt::cl::maximum<int>()
+		* bolt::cl::sort(ctl, a, a+10);
+		* // a => {2, 3, 3, 3, 4, 5, 6, 7, 8, 9}
 		*  \endcode
 		*/
 		template<typename RandomAccessIterator> 
@@ -105,83 +102,70 @@ namespace bolt {
 			const std::string cl_code="");
 
 
+		/*! \p sort returns the sorted result of all the elements in the inputIterator between the the first and last elements using the specified binary_op.  
+		* You can arrange the elements in an ascending order, where the binary_op is the minimum operator.  By default, 
+		* the binary operator is "minimum<>()".  The version takes a bolt::cl::control structure as a first argument.
 
-
-		/*! \p reduce returns the result of combining all the elements in the specified range using the specified binary_op.  
-		* The classic example is a summation, where the binary_op is the plus operator.  By default, 
-		* the binary operator is "plus<>()".
 		*
-		* \p reduce requires that the binary reduction op ("binary_op") is cummutative.  The order in which \p reduce applies the binary_op
-		* is not deterministic.
+		* The \p sort operation is similar the std::sort function.  See http://www.sgi.com/tech/stl/sort.html
 		*
-		* The \p reduce operation is similar the std::accumulate function.  See http://www.sgi.com/tech/stl/accumulate.html.
-		*
-		* \param first The first position in the sequence to be reduced.
-		* \param last  The last position in the sequence to be reduced.
-		* \param init  The initial value for the accumulator.
-		* \param binary_op  The binary operation used to combine two values.   By default, the binary operation is plus<>().
+		* \param first The first position in the sequence to be sorted.
+		* \param last  The last position in the sequence to be sorted.
 		* \param cl_code Optional OpenCL(TM) code to be passed to the OpenCL compiler. The cl_code is inserted first in the generated code, before the cl_code trait.
-		* \return The result of the reduction.
+		* \return The sorted data which is available in place and is in ascending order.
 		*
-		* The following code example shows the use of \p reduce to sum 10 numbers plus 100, using the default plus operator.
+		* Here also the elements are arranged in ascending order. The routine takes care of allocating the cl::CommandQueue an other data structures. 
+        * The following code example shows the use of \p sort to sort the elements in the ascending order, 
+		* specifying a specific command-queue and enabling debug messages.
 		* \code
-		* #include <bolt/cl/reduce.h>
-		*
-		* int a[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-		*
-		* int sum = bolt::cl::reduce(a, a+10, 100);
-		* // sum = 155
-		*  \endcode
-		*
-		* The following code example shows the use of \p reduce to find the max of 10 numbers:
-		* \code
-		* #include <bolt/cl/reduce.h>
+		* #include <bolt/cl/sort.h>
 		*
 		* int a[10] = {2, 9, 3, 7, 5, 6, 3, 8, 3, 4};
 		*
-		* int max = bolt::cl::reduce(a, a+10, -1, bolt::cl:maximum<int>());
-		* // max = 9
+		* // for arranging th eelements in the descending order, you can use bolt::cl::maximum<int>()
+		* bolt::cl::sort(a, a+10);
+		* // a => {2, 3, 3, 3, 4, 5, 6, 7, 8, 9}
 		*  \endcode
 		*/
-		template<typename RandomAccessIterator, typename Compare> 
+        
+        template<typename RandomAccessIterator, typename Compare> 
 		void sort(RandomAccessIterator first, 
 			RandomAccessIterator last,  
             Compare comp=bolt::cl::minimum<typename std::iterator_traits<RandomAccessIterator>::value_type>(), 
-			const std::string cl_code="")  ;
+			const std::string cl_code="");
 
 
-		/*! \p reduce returns the result of combining all the elements in the specified range using the specified binary_op.  
-		* The classic example is a summation, where the binary_op is the plus operator.  By default, the initial value is "0" 
-		* and the binary operator is "plus<>()".
+		/*! \p sort returns the sorted result of all the elements in the inputIterator between the the first and last elements using the specified binary_op.  
+		* You can arrange the elements in an ascending order, where the binary_op is the minimum operator.  By default, 
+		* the binary operator is "minimum<>()".  The version takes a bolt::cl::control structure as a first argument.
+
 		*
-		* \p reduce requires that the binary reduction op ("binary_op") is cummutative.  The order in which \p reduce applies the binary_op
-		* is not deterministic.
+		* The \p sort operation is similar the std::sort function.  See http://www.sgi.com/tech/stl/sort.html
 		*
-		* The \p reduce operation is similar the std::accumulate function.  See http://www.sgi.com/tech/stl/accumulate.html.
-		*
-		* \param first The first position in the sequence to be reduced.
-		* \param last  The last position in the sequence to be reduced.
+		* \param first The first position in the sequence to be sorted.
+		* \param last  The last position in the sequence to be sorted.
 		* \param cl_code Optional OpenCL(TM) code to be passed to the OpenCL compiler. The cl_code is inserted first in the generated code, before the cl_code trait.
-		* \return The result of the reduction.
+		* \return The sorted data which is available in place and is in ascending order.
 		*
-		* The following code example shows the use of \p reduce to sum 10 numbers, using the default plus operator.
+		* Here also the elements are arranged in ascending order. The routine takes care of allocating the cl::CommandQueue an other data structures. 
+        * The following code example shows the use of \p sort to sort the elements in the ascending order, 
+		* specifying a specific command-queue and enabling debug messages.
 		* \code
-		* #include <bolt/cl/reduce.h>
+		* #include <bolt/cl/sort.h>
 		*
-		* int a[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		* int a[10] = {2, 9, 3, 7, 5, 6, 3, 8, 3, 4};
 		*
-		* int sum = bolt::cl::reduce(a, a+10);
-		* // sum = 55
+		* // for arranging the elements in the descending order, you can use bolt::cl::maximum<int>()
+		* bolt::cl::sort(a, a+10);
+		* // a => {2, 3, 3, 3, 4, 5, 6, 7, 8, 9}
 		*  \endcode
-		*
 		*/
 		template<typename RandomAccessIterator> 
 		void sort(RandomAccessIterator first, 
 			RandomAccessIterator last, 
 			const std::string cl_code="");
-
-	}
-}
+	}// end of bolt::cl namespace
+}// end of bolt namespace
 
 #include <bolt/cl/detail/sort.inl>
 
