f440e153b453ca130ff45d78f1cf15cdfee326d3
Kent Knox (xN/A) TX <kent.knox@amd.com>
2012-09-01 00:56:16 -0600

    ECR #361231 - Added INSTALL target logic to copy our dependent Boost headers into the SDK. To test the Bolt SDK contents, I created a trivial Scan sample in the examples folder and included it in the SDK; it is treated like an independant app that includes includes header and library files from the contents of the SDK.  The sample compiles with cmake too, so I wrote a basic FindBolt.cmake file, to help users who want to use cmake to find the Bolt header and library files. Fixed a few typos in the documentation.
    [git-p4: depot-paths = "//depot/stg/hsa/drivers/hsa/boltProject/": change = 812749]

---
+++ examples/FindBolt.cmake
@@ -0,0 +1,93 @@
+# Locate an BOLT implementation.
+#
+# Defines the following variables:
+#
+#   BOLT_FOUND - Found an Bolt imlementation
+#
+# Also defines the library variables below as normal
+# variables.
+#
+#   BOLT_LIBRARIES - These contain debug/optimized keywords when a debugging library is found
+#   BOLT_INCLUDE_DIRS - All relevant Bolt include directories
+#
+# Accepts the following variables as input:
+#
+#   BOLT_ROOT - (as a CMake or environment variable)
+#                The root directory of an BOLT installation
+#
+#   FIND_LIBRARY_USE_LIB64_PATHS - Global property that controls whether FindBOLT should search for 
+#                              64bit or 32bit libs
+#
+#-----------------------
+# Example Usage:
+#
+#    find_package(BOLT REQUIRED)
+#    include_directories(${BOLT_INCLUDE_DIRS})
+#
+#    add_executable(foo foo.cc)
+#    target_link_libraries(foo ${BOLT_LIBRARIES})
+#
+#-----------------------
+
+# This module helps to use BOLT_FIND_COMPONENTS, BOLT_FIND_REQUIRED, BOLT_FIND_QUIETLY
+include( FindPackageHandleStandardArgs )
+
+# Search for 64bit libs if FIND_LIBRARY_USE_LIB64_PATHS is set to true in the global environment, 32bit libs else
+get_property( LIB64 GLOBAL PROPERTY FIND_LIBRARY_USE_LIB64_PATHS )
+
+# Debug print statements
+#message( "BOLT_LIBRARY_PATH_SUFFIXES: ${BOLT_LIBRARY_PATH_SUFFIXES}" )
+#message( "ENV{BOLT_ROOT}: $ENV{BOLT_ROOT}" )
+#message( "BOLT_FIND_COMPONENTS: ${BOLT_FIND_COMPONENTS}" )
+#message( "BOLT_FIND_REQUIRED: ${BOLT_FIND_REQUIRED}" )
+
+# Set the component to find if the user does not specify explicitely
+if( NOT BOLT_FIND_COMPONENTS )
+	set( BOLT_FIND_COMPONENTS CL )
+endif( )
+
+# Eventually, Bolt may support multiple backends, but for now it only supports CL
+list( FIND BOLT_FIND_COMPONENTS CL find_CL )
+if( NOT find_CL EQUAL -1 )
+	set( BOLT_LIBNAME_STATIC clBolt.Runtime.lib )
+endif( )
+
+if( LIB64 )
+	set( archPath "lib64" )
+else( )
+	set( archPath "lib" )
+endif( )
+
+if( NOT find_CL EQUAL -1 )
+	# Find and set the location of main BOLT static lib file
+	find_library( BOLT_LIBRARY_STATIC
+		NAMES ${BOLT_LIBNAME_STATIC}
+		HINTS
+			${BOLT_ROOT}
+			ENV BOLT_ROOT
+		DOC "BOLT static library path"
+		PATH_SUFFIXES lib/import
+	)
+	mark_as_advanced( BOLT_LIBRARY_STATIC )
+
+    find_path( BOLT_INCLUDE_DIRS
+        NAMES bolt/cl/bolt.h
+        HINTS
+            ${BOLT_ROOT}
+            ENV BOLT_ROOT
+        DOC "BOLT header file path"
+        PATH_SUFFIXES include
+    )
+    mark_as_advanced( BOLT_INCLUDE_DIRS )
+
+	FIND_PACKAGE_HANDLE_STANDARD_ARGS( BOLT DEFAULT_MSG BOLT_LIBRARY_STATIC BOLT_INCLUDE_DIRS )
+endif( )
+
+if( BOLT_FOUND )
+	list( APPEND BOLT_LIBRARIES ${BOLT_LIBRARY_STATIC} )
+else( )
+	if( NOT BOLT_FIND_QUIETLY )
+		message( WARNING "FindBOLT could not find the BOLT library" )
+		message( STATUS "Did you remember to set the BOLT_ROOT environment variable?" )
+	endif( )
+endif()
---
+++ examples/Scan/scan.cpp
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+
+#include <bolt/cl/scan.h>
+
+#include <vector>
+#include <numeric>
+
+int _tmain( int argc, _TCHAR* argv[ ] )
+{
+    bolt::cl::device_vector< int > boltInput( 1024, 1 );
+    bolt::cl::device_vector< int >::iterator boltEnd = bolt::cl::inclusive_scan( boltInput.begin( ), boltInput.end( ), boltInput.begin( ) );
+
+    std::vector< int > stdInput( 1024, 1 );
+    std::vector< int >::iterator stdEnd  = bolt::cl::inclusive_scan( stdInput.begin( ), stdInput.end( ), stdInput.begin( ) );
+    
+	return 0;
+}
