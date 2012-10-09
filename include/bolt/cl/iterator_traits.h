0034b47272e9e8dd420fec78152ecefc6ac7e6c1
Ravishekhar Banger (xN/A) BG <ravishekhar.banger@amd.com>
2012-10-09 05:30:53 -0600

    ECR #361231 - Fixed the EPR's 363485, 362322, 363467
    [git-p4: depot-paths = "//depot/stg/hsa/drivers/hsa/boltProject/": change = 829043]

--- include/bolt/cl/iterator_traits.h
+++ include/bolt/cl/iterator_traits.h
@@ -24,8 +24,8 @@ namespace bolt {
 		typedef int ptrdiff_t; // Map all pointer differences to 32-bit integers for OCL benefit.
 
 		template <class Iterator> struct iterator_traits {
-			//typedef typename Iterator::difference_type difference_type;
-			typedef int difference_type;  // FIXME, this should call down to the lower-level iterators.
+			typedef typename Iterator::difference_type difference_type;
+			//typedef int difference_type;  // FIXME, this should call down to the lower-level iterators.
 			typedef typename Iterator::value_type value_type;
 			typedef typename Iterator::pointer pointer;
 			typedef typename Iterator::reference reference;
--- test/cl/CountTest/CountTest.cpp
+++ test/cl/CountTest/CountTest.cpp
@@ -15,8 +15,251 @@
 
 ***************************************************************************/                                                                                     
 
-// TransformTest.cpp : Defines the entry point for the console application.
+#define TEST_DOUBLE 0
+#define TEST_DEVICE_VECTOR 1
+#define TEST_CPU_DEVICE 0
+#define GOOGLE_TEST 1
+#if (GOOGLE_TEST == 1)
+#include "common/stdafx.h"
+#include "common/myocl.h"
+
+#include <bolt/cl/count.h>
+#include <bolt/cl/functional.h>
+#include <bolt/miniDump.h>
+#include <bolt/unicode.h>
+
+#include <gtest/gtest.h>
+#include <boost/shared_array.hpp>
+#include <array>
+#include <algorithm>
+class testCountIfFloatWithStdVector: public ::testing::TestWithParam<int>{
+protected:
+	int aSize;
+public:
+	testCountIfFloatWithStdVector():aSize(GetParam()){
+	}
+	
+};
+
+
+std::string InRange_CodeString = 
+BOLT_CODE_STRING(
+template<typename T>
+// Functor for range checking.
+struct InRange {
+	InRange (T low, T high) {
+		_low=low;
+		_high=high;
+	};
+
+	bool operator() (const T& value) { 
+		//printf("Val=%4.1f, Range:%4.1f ... %4.1f\n", value, _low, _high); 
+		return (value >= _low) && (value <= _high) ; 
+	};
+
+	T _low;
+	T _high;
+};
+);
+
+BOLT_CREATE_TYPENAME(InRange<int>);
+BOLT_CREATE_CLCODE(InRange<int>, InRange_CodeString);
+BOLT_CREATE_TYPENAME(InRange<float>);
+BOLT_CREATE_CLCODE(InRange<float>, InRange_CodeString);
+BOLT_CREATE_TYPENAME(InRange<double>);
+BOLT_CREATE_CLCODE(InRange<double>, InRange_CodeString);
+
+
+TEST_P (testCountIfFloatWithStdVector, countFloatValueInRange)
+{
+	std::vector<float> A(aSize);
+	std::vector<float> B(aSize);
+
+	for (int i=0; i < aSize; i++) {
+		A[i] = static_cast<float> (i+1);
+		B[i] = A[i];
+	};
+
+	size_t stdCount = std::count_if (A.begin(), A.end(), InRange<float>(6,10)) ;
+	size_t boltCount = bolt::cl::count_if (B.begin(), B.end(), InRange<float>(6,10)) ;
+
+	EXPECT_EQ (stdCount, boltCount)<<"Failed as: STD Count = "<<stdCount<<"\nBolt Count = "<<boltCount<<std::endl;
+	std::cout<<"STD Count = "<<stdCount<<std::endl<<"bolt Count = "<<boltCount<<std::endl;
+}
+
+TEST_P (testCountIfFloatWithStdVector, countFloatValueInRange2)
+{
+	std::vector<float> A(aSize);
+	std::vector<float> B(aSize);
+
+	for (int i=0; i < aSize; i++) {
+		A[i] = static_cast<float> (i+1);
+		B[i] = A[i];
+	};
+
+	size_t stdCount = std::count_if (A.begin(), A.end(), InRange<float>(1,10)) ;
+	size_t boltCount = bolt::cl::count_if (B.begin(), B.end(), InRange<float>(1,10)) ;
+
+	EXPECT_EQ (stdCount, boltCount)<<"Failed as: STD Count = "<<stdCount<<"\nBolt Count = "<<boltCount<<std::endl;
+	std::cout<<"STD Count = "<<stdCount<<std::endl<<"bolt Count = "<<boltCount<<std::endl;
+}
+
 //
+//Test case id: 6 (FAILED)
+
+class countFloatValueOccuranceStdVect :public ::testing::TestWithParam<int>{
+protected:
+	int stdVectSize;
+public:
+	countFloatValueOccuranceStdVect():stdVectSize(GetParam()){}
+};
+
+TEST_P(countFloatValueOccuranceStdVect, floatVectSearchWithSameValue){
+	std::vector<float> stdVect(stdVectSize);
+	std::vector<float> boltVect(stdVectSize);
+	
+	float myFloatValue = 1.23f;
+
+	for (int i =0; i < stdVectSize; i++){
+		stdVect[i] = myFloatValue;
+		boltVect[i] = stdVect[i];
+	}
+
+	size_t stdCount = std::count(stdVect.begin(), stdVect.end(), myFloatValue);
+	size_t boltCount = bolt::cl::count(boltVect.begin(), boltVect.end(), myFloatValue);
+
+	EXPECT_EQ(stdCount, boltCount)<<"Failed as: \nSTD Count = "<<stdCount<<std::endl<<"Bolt Count = "<<boltCount<<std::endl;
+	std::cout<<"STD Count = "<<stdCount<<std::endl<<"Bolt Count = "<<boltCount<<std::endl;
+}
+
+TEST_P(countFloatValueOccuranceStdVect, floatVectSearchWithSameValue2){
+	std::vector<float> stdVect(stdVectSize);
+	std::vector<float> boltVect(stdVectSize);
+	
+	float myFloatValue2 = 9.87f;
+
+	for (int i =0; i < stdVectSize; i++){
+		stdVect[i] = myFloatValue2;
+		boltVect[i] = stdVect[i];
+	}
+
+	size_t stdCount = std::count(stdVect.begin(), stdVect.end(), myFloatValue2);
+	size_t boltCount = bolt::cl::count(boltVect.begin(), boltVect.end(), myFloatValue2);
+
+	EXPECT_EQ(stdCount, boltCount)<<"Failed as: \nSTD Count = "<<stdCount<<std::endl<<"Bolt Count = "<<boltCount<<std::endl;
+	std::cout<<"STD Count = "<<stdCount<<std::endl<<"Bolt Count = "<<boltCount<<std::endl;
+}
+
+INSTANTIATE_TEST_CASE_P (useStdVectWithFloatValues, countFloatValueOccuranceStdVect, ::testing::Values(1, 100, 1000, 10000, 100000));
+
+
+//Test case id: 7 (Failed)
+class countDoubleValueUsedASKeyInStdVect :public ::testing::TestWithParam<int>{
+protected:
+	int stdVectSize;
+public:
+	countDoubleValueUsedASKeyInStdVect():stdVectSize(GetParam()){}
+};
+
+
+TEST_P(countDoubleValueUsedASKeyInStdVect, doubleVectSearchWithSameValue){
+	std::vector<double> stdVect(stdVectSize);
+	std::vector<double> boltVect(stdVectSize);
+	
+	double myDoubleValueAsKeyValue = 1.23456789l;
+
+	for (int i =0; i < stdVectSize; i++){
+		stdVect[i] = myDoubleValueAsKeyValue;
+		boltVect[i] = stdVect[i];
+	}
+
+	size_t stdCount = std::count(stdVect.begin(), stdVect.end(), myDoubleValueAsKeyValue);
+	size_t boltCount = bolt::cl::count(boltVect.begin(), boltVect.end(), myDoubleValueAsKeyValue);
+
+	EXPECT_EQ(stdCount, boltCount)<<"Failed as: \nSTD Count = "<<stdCount<<std::endl<<"Bolt Count = "<<boltCount<<std::endl;
+	std::cout<<"STD Count = "<<stdCount<<std::endl<<"Bolt Count = "<<boltCount<<std::endl;
+}
+
+TEST_P(countDoubleValueUsedASKeyInStdVect, doubleVectSearchWithSameValue2){
+	std::vector<double> stdVect(stdVectSize);
+	std::vector<double> boltVect(stdVectSize);
+	
+	double myDoubleValueAsKeyValue2 = 9.876543210123456789l;
+
+	for (int i =0; i < stdVectSize; i++){
+		stdVect[i] = myDoubleValueAsKeyValue2;
+		boltVect[i] = stdVect[i];
+	}
+
+	size_t stdCount = std::count(stdVect.begin(), stdVect.end(), myDoubleValueAsKeyValue2);
+	size_t boltCount = bolt::cl::count(boltVect.begin(), boltVect.end(), myDoubleValueAsKeyValue2);
+
+	EXPECT_EQ(stdCount, boltCount)<<"Failed as: \nSTD Count = "<<stdCount<<std::endl<<"Bolt Count = "<<boltCount<<std::endl;
+	std::cout<<"STD Count = "<<stdCount<<std::endl<<"Bolt Count = "<<boltCount<<std::endl;
+}
+
+//test case: 1, test: 1
+TEST (testCountIf, intBtwRange) 
+{
+	int aSize = 1024;
+	std::vector<int> A(aSize);
+	
+	for (int i=0; i < aSize; i++) {
+		A[i] = rand() % 10 + 1;
+	}
+
+	std::iterator_traits<std::vector<int>::iterator>::difference_type stdInRangeCount = std::count_if (A.begin(), A.end(), InRange<int>(1,10)) ;
+	std::iterator_traits<std::vector<int>::iterator>::difference_type boltInRangeCount = bolt::cl::count_if (A.begin(), A.end(), InRange<int>(1, 10)) ;
+
+	EXPECT_EQ(stdInRangeCount, boltInRangeCount);
+}
+
+
+INSTANTIATE_TEST_CASE_P (useStdVectWithDoubleValues, countDoubleValueUsedASKeyInStdVect, ::testing::Values(1, 100, 1000, 10000, 100000));
+INSTANTIATE_TEST_CASE_P (serialFloatValueWithStdVector, testCountIfFloatWithStdVector, ::testing::Values(10, 100, 1000, 10000, 100000));
+
+int main(int argc, char* argv[])
+{
+    ::testing::InitGoogleTest( &argc, &argv[ 0 ] );
+
+    //  Register our minidump generating logic
+    bolt::miniDumpSingleton::enableMiniDumps( );
+
+    int retVal = RUN_ALL_TESTS( );
+
+    //  Reflection code to inspect how many tests failed in gTest
+    ::testing::UnitTest& unitTest = *::testing::UnitTest::GetInstance( );
+
+    unsigned int failedTests = 0;
+    for( int i = 0; i < unitTest.total_test_case_count( ); ++i )
+    {
+        const ::testing::TestCase& testCase = *unitTest.GetTestCase( i );
+        for( int j = 0; j < testCase.total_test_count( ); ++j )
+        {
+            const ::testing::TestInfo& testInfo = *testCase.GetTestInfo( j );
+            if( testInfo.result( )->Failed( ) )
+                ++failedTests;
+        }
+    }
+
+    //  Print helpful message at termination if we detect errors, to help users figure out what to do next
+    if( failedTests )
+    {
+        bolt::tout << _T( "\nFailed tests detected in test pass; please run test again with:" ) << std::endl;
+        bolt::tout << _T( "\t--gtest_filter=<XXX> to select a specific failing test of interest" ) << std::endl;
+        bolt::tout << _T( "\t--gtest_catch_exceptions=0 to generate minidump of failing test, or" ) << std::endl;
+        bolt::tout << _T( "\t--gtest_break_on_failure to debug interactively with debugger" ) << std::endl;
+        bolt::tout << _T( "\t    (only on googletest assertion failures, not SEH exceptions)" ) << std::endl;
+    }
+    std::cout << "Test Completed. Press Enter to exit.\n .... ";
+    getchar();
+	return retVal;
+}
+
+
+#else
+
+
 
 #include "stdafx.h"
 #include <vector>
@@ -128,3 +371,4 @@ int _tmain(int argc, _TCHAR* argv[])
 	return 0;
 }
 
+#endif
