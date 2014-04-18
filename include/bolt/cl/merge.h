fe34bd88bd3e2d92577f86012fe646d7e9b29eef
rkskvk <rajendraiisc@gmail.com>
2014-04-18 10:51:53 +0530

    Correct Bolt Documentaition and shift the Merge routines to the Merging group under the Algorithms group

--- include/bolt/cl/merge.h
+++ include/bolt/cl/merge.h
@@ -33,18 +33,17 @@ namespace bolt {
 
         /*! \addtogroup algorithms
          */
-
-        /*! \addtogroup sort
+		
+		/*! \addtogroup merging
         *   \ingroup algorithms
-        *
         */
 
         /*! \addtogroup CL-merge
-        *   \ingroup sorting
+        *   \ingroup merging
         *   \{
         */
 
-
+		
         /*! \brief \p merge returns the result of combining the two sorted range [first1, last1] and [first2, last2] in
         * to a single sorted range [result , result + (last1-first1) + ( last2-first2)]
         *
